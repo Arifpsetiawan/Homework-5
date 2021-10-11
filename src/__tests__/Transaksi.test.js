a2ead793ea9a13adde627b9a2f8b41c9f9c0c627
Arif Puji Setiawan <arif.p.setiawan.9@gmail.com>
2021-10-11 19:22:09 +0700

    Add enzyme-adapter-react-17

--- src/__tests__/Transaksi.test.js
+++ src/__tests__/Transaksi.test.js
@@ -1,22 +1,27 @@
-import { screen, waitFor } from "@testing-library/react"
-import { QueryClient, QueryClientProvider } from "react-query"
+import React from "react"
 import Enzyme, { shallow } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
 
 import TransaksiPage from "../pages/Transaksi/TransaksiPage"
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe("TransaksiPage", () => {
-  test("has a button", () => {
+  it("renders with Result", () => {
     const wrapper = shallow(<TransaksiPage />)
-    expect(wrapper.find("Button")).toHaveLength(1)
-    expect(wrapper.find("Button").text()).toEqual("Cari Agen")
+    expect(wrapper.find("Option").length).toBe(1)
+    expect(wrapper.find("Option").prop("value")).toBe("Report")
   })
-  test("renders modal when visible is true", () => {
-    const props = { visible: true }
-    const wrapper = shallow(<TransaksiPage {...props} />)
-    wrapper.update()
-    expect(wrapper.find(".searching-agent").exists()).toEqual(true)
-  })
+
+  // test("has a button", () => {
+  //   const wrapper = shallow(<TransaksiPage />)
+  //   expect(wrapper.find("Button")).toHaveLength(1)
+  //   expect(wrapper.find("Button").text()).toEqual("Cari Agen")
+  // })
+  // test("renders modal when visible is true", () => {
+  //   const props = { visible: true }
+  //   const wrapper = shallow(<TransaksiPage {...props} />)
+  //   wrapper.update()
+  //   expect(wrapper.find(".searching-agent").exists()).toEqual(true)
+  // })
 })
